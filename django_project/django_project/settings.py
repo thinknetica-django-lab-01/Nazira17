2a169ad644af2a45beb9062cfd134332a001e916
Nazira <you@example.com>
2021-01-19 16:39:02 +0600

    Add index.html and about, contacts

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_project',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'django_project.urls'
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+SITE_ID = 1
--- django_project/django_project/tests/functional/test_django.py
+++ django_project/django_project/tests/functional/test_django.py
@@ -1,4 +1,13 @@
 
-def test_django(client):
-    response = client.get('/')
-    assert response.status_code == 404
+# def test_about(client):
+#     response = client.get('/about/')
+#     assert response.status_code == 200
+#     response = response.content.decode('utf-8')
+#     assert 'Текст о нас' in response
+#
+#
+# def test_contacts(client):
+#     response = client.get('/contacts/')
+#     assert response.status_code == 200
+#     response = response.content.decode('utf-8')
+#     assert 'Контакты' in response
