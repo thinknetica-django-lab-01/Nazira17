610d0b51c8f459344544e2d7e24adf171d259866
Nazira <you@example.com>
2021-02-18 16:57:10 +0600

    Add tasks.py and celery.py

--- django_project/main/tasks.py
+++ django_project/main/tasks.py
@@ -5,19 +5,19 @@ from django.template.loader import render_to_string
 from datetime import datetime, timedelta
 
 
-# @shared_task()
-# def send_new_good():
-#     subscribers = Subscriber.objects.values("subscriber__email")
-#     today = datetime.today()
-#     products = Product.objects.filter(add_date__lte=today, add_date__gt=today - timedelta(days=7))
-#     subject = 'Check out the new product'
-#     message = render_to_string('account/email/new_good.html', {
-#         'product': products,
-#         'name': Subscriber.subscriber,
-#     })
-#     text_content = render_to_string("account/email/new_good.txt", {"name": Subscriber.subscriber})
-#     email = EmailMultiAlternatives(subject, text_content, from_email='nazira.yegizbayeva@gmail.com',
-#                                    to=[subscribers])
-#     email.attach_alternative(message, 'text/html')
-#     email.send()
-#     print('sended')
+@shared_task()
+def send_new_good():
+    subscribers = Subscriber.objects.values("subscriber__email")
+    today = datetime.today()
+    products = Product.objects.filter(add_date__lte=today, add_date__gt=today - timedelta(days=7))
+    subject = 'Check out the new product'
+    message = render_to_string('account/email/new_good.html', {
+        'product': products,
+        'name': Subscriber.subscriber,
+    })
+    text_content = render_to_string("account/email/new_good.txt", {"name": Subscriber.subscriber})
+    email = EmailMultiAlternatives(subject, text_content, from_email='nazira.yegizbayeva@gmail.com',
+                                   to=[subscribers])
+    email.attach_alternative(message, 'text/html')
+    email.send()
+    print('sended')
