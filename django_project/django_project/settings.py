8c0768f8880df2040a9d7c4968b98fb0262813f6
Nazira <you@example.com>
2021-02-18 16:52:13 +0600

    Add celery.py and tasks.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Celery Configuration Options
+CELERY_TIMEZONE = "UTC"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_RESULT_BACKEND = "django-db"
 
 # Application definition
 
@@ -46,6 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.flatpages',
     'main',
     'ckeditor',
+    'django_celery_beat',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
--- django_project/main/services.py
+++ django_project/main/services.py
@@ -1,27 +1,27 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from django.core.mail import EmailMultiAlternatives
-from .models import *
-from django.template.loader import render_to_string
-from datetime import datetime, timedelta
-
-
-def start():
-
-    def send_new_good():
-        subscribers = Subscriber.objects.values("subscriber__email")
-        today = datetime.today()
-        products = Product.objects.filter(add_date__lte=today, add_date__gt=today - timedelta(days=7))
-        subject = 'Check out the new product'
-        message = render_to_string('account/email/new_good.html', {
-            'product': products,
-            'name': Subscriber.subscriber,
-        })
-        text_content = render_to_string("account/email/new_good.txt", {"name": Subscriber.subscriber})
-        email = EmailMultiAlternatives(subject, text_content, from_email='nazira.yegizbayeva@gmail.com',
-                                       to=[subscribers])
-        email.attach_alternative(message, 'text/html')
-        email.send()
-        print('sended')
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(send_new_good, 'interval', seconds=10)
-    scheduler.start()
+# from apscheduler.schedulers.background import BackgroundScheduler
+# from django.core.mail import EmailMultiAlternatives
+# from .models import *
+# from django.template.loader import render_to_string
+# from datetime import datetime, timedelta
+#
+#
+# def start():
+#
+#     def send_new_good():
+#         subscribers = Subscriber.objects.values("subscriber__email")
+#         today = datetime.today()
+#         products = Product.objects.filter(add_date__lte=today, add_date__gt=today - timedelta(days=7))
+#         subject = 'Check out the new product'
+#         message = render_to_string('account/email/new_good.html', {
+#             'product': products,
+#             'name': Subscriber.subscriber,
+#         })
+#         text_content = render_to_string("account/email/new_good.txt", {"name": Subscriber.subscriber})
+#         email = EmailMultiAlternatives(subject, text_content, from_email='nazira.yegizbayeva@gmail.com',
+#                                        to=[subscribers])
+#         email.attach_alternative(message, 'text/html')
+#         email.send()
+#         print('sended')
+#     scheduler = BackgroundScheduler()
+#     scheduler.add_job(send_new_good, 'interval', seconds=10)
+#     scheduler.start()
